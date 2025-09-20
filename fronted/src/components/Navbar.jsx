08d37b6b4a53cd163c4de96f109e49560c360ee6
BholaCraft07 <bholakumar1990kp@gmail.com>
2025-09-20 06:55:12 +0530

    url updation

--- fronted/src/components/Navbar.jsx
+++ fronted/src/components/Navbar.jsx
@@ -10,22 +10,13 @@ export default function Navbar() {
                 <span
                     className={`flex items-center gap-[4px] sm:gap-[6px] font-bold tracking-tight transition-colors duration-300 text-lg sm:text-xl `}
                 >
-                    {/* <Image
-                        src="/favicon.svg"
-                        alt="PatternCraft Logo"
-                        width={20}
-                        height={20}
-                        className="flex-shrink-0 mt-[2px] h-6 w-6 sm:h-6 sm:w-6"
-                        priority
-                        draggable={false}
-                        style={{ userSelect: "none" }}
-                    /> */}
+                    
                     <span className="whitespace-nowrap">BookStore</span>
                 </span>
                 <div className="flex items-center gap-3">
                     <div className="flex gap-2 sm:gap-4">
                         <a
-                            href="#"
+                            href="https://github.com/bh-fsdev/book-explorer"
                             target="_blank"
                             rel="noopener noreferrer"
                             className={`rounded-full p-1.5 sm:p-2 transition-all duration-300 `}
